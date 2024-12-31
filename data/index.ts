f8f3cde9583b294f083b84918fbf52a181e69b32
Yarzadeh <hosein.yarzade@gmail.com>
2024-12-31 23:54:10 +0330

    add Grid BentoGrid

--- data/index.ts
+++ data/index.ts
@@ -4,3 +4,29 @@ export const navItems = [
     { name: "Testimonials", link: "#testimonials" },
     { name: "Contact", link: "#contact" },
   ];
+
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+
+  ];
+  export const projects = [
+    {
+      id: 1,
+      title: "DaraCard",
+      des: "BMPL projectover than 3000 users created with react.js ,PWA ",
+      img: "/p1.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/fm.svg"],
+      link: "https://mobile.daracard.co/",
+    },
+
+  ];
+  
